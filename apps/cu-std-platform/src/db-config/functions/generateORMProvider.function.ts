f2bbbdd96fb870603d33aaad95e315966c8ba7e8
Elizbarashvili Guram <elizbarashvili.guram18@gmail.com>
2024-05-26 20:37:02 +0400

    edited: docker-compose.yml

--- apps/cu-std-platform/src/db-config/functions/generateORMProvider.function.ts
+++ apps/cu-std-platform/src/db-config/functions/generateORMProvider.function.ts
@@ -1,26 +1,35 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
-import process from 'node:process';
 
 export function generateORMProvider() {
-  return process.env.isProd
-    ? TypeOrmModule.forRoot({
-        type: 'postgres',
-        host: 'db', // Use the Docker service name as the host
-        port: 5432, // Use the internal Docker port for PostgreSQL
-        username: 'jigar',
-        password: 'mysecretpassword',
-        database: 'mydb', // Ensure this matches the DB name given in Docker Compose
-        synchronize: true,
-        autoLoadEntities: true,
-      })
-    : TypeOrmModule.forRoot({
-        type: 'postgres',
-        host: 'localhost',
-        port: 8080,
-        username: 'jigar',
-        password: 'mysecretpassword',
-        database: 'jigar',
-        synchronize: true,
-        autoLoadEntities: true,
-      });
+  return TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'db', // Use the Docker service name as the host
+    port: 5432, // Use the internal Docker port for PostgreSQL
+    username: 'jigar',
+    password: 'mysecretpassword',
+    database: 'mydb', // Ensure this matches the DB name given in Docker Compose
+    synchronize: true,
+    autoLoadEntities: true,
+  });
+  // return process.env.isProd
+  //   ? TypeOrmModule.forRoot({
+  //       type: 'postgres',
+  //       host: 'db', // Use the Docker service name as the host
+  //       port: 5432, // Use the internal Docker port for PostgreSQL
+  //       username: 'jigar',
+  //       password: 'mysecretpassword',
+  //       database: 'mydb', // Ensure this matches the DB name given in Docker Compose
+  //       synchronize: true,
+  //       autoLoadEntities: true,
+  //     })
+  //   : TypeOrmModule.forRoot({
+  //       type: 'postgres',
+  //       host: 'localhost',
+  //       port: 8080,
+  //       username: 'jigar',
+  //       password: 'mysecretpassword',
+  //       database: 'jigar',
+  //       synchronize: true,
+  //       autoLoadEntities: true,
+  //     });
 }
